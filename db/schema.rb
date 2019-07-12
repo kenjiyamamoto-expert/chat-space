e674c2df44ef984475baee6e777348f07999303d
kenji <totyu_go2y@yahoo.co.jp>
2019-07-12 14:29:52 +0900

    third-4 commit

--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,4 @@
 
 ActiveRecord::Schema.define(version: 20190711111211) do
 
-  create_table "articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "title"
-    t.string   "author"
-    t.text     "text",       limit: 65535
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-  end
-
 end
