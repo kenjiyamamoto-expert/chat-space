21a26f09c6f74cda88e810cb1a2c4d2b3d77f402
kenji <totyu_go2y@yahoo.co.jp>
2019-07-26 20:46:20 +0900

    first-commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
-  # ユーザー情報編集機能を実装
-  resources :users, only: [:edit, :update]
+  # ユーザー情報編集機能とユーザー検索機能(index)を実装
+  resources :users, only: [:index,:edit, :update]
+
   # グループ機能の実装
   resources :groups, only: [:new, :create, :edit, :update] do
     # メッセージ送信機能の実装
