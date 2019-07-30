ca843e5c53b068ba405fee37c11aed44c7506436
kenji <totyu_go2y@yahoo.co.jp>
2019-07-30 16:59:36 +0900

    5-commit

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,8 +1,6 @@
 class Api::MessagesController < ApplicationController
   def index
-    #今いるグループの情報をパラムスの値を元にDBから取得。
-    #グループが所有しているメッセージの中から、params[:last_id]よりも大きいidがないかMessageから検索して、@messagesに代入。
+    #グループが所有しているメッセージの中から、group_idが一致した物をDBから取得し、かつparams[:last_id]よりも大きいidがないかMessageから検索して、@messagesに代入。
     @messages = Message.includes(:user).where('id > ? AND group_id = ?', params[:id], params[:group_id])
-    
   end
 end
